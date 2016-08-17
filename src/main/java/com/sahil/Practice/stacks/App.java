57c5e167b6182fd4ca52a63ac61244e2b7c7610f
Sahil Maggu <sahil_maggu@intuit.com>
2016-08-17 19:48:11 +0530

    First Commit

---
+++ src/main/java/com/sahil/Practice/stacks/App.java
@@ -0,0 +1,13 @@
+package com.sahil.Practice.stacks;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
